f6a50322855872d691c7bb17965a5705a2788bfc
deano <me@deanoc.com>
2019-07-24 10:49:53 +0300

    rename gfx_imageformat to tiny_imageformat some windows fixes to imagecompress and runner

--- tests/test_image.cpp
+++ tests/test_image.cpp
@@ -1,6 +1,6 @@
 #include "al2o3_platform/platform.h"
 #include "gfx_image/image.h"
-#include "gfx_imageformat/formatcracker.h"
+#include "tiny_imageformat/formatcracker.h"
 #include "gfx_image/create.h"
 #include "al2o3_catch2/catch2.hpp"
 
