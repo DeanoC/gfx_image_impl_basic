d1da13ae4c6ae142688c220e05f4b1527349b696
Dean Calver <me@deanoc.com>
2019-08-20 17:35:20 +0300

    Tidying Image stuff, adding comments etc. waiting for a build...

--- src/create.cpp
+++ src/create.cpp
@@ -1,8 +1,102 @@
 #include "al2o3_platform/platform.h"
+#include "al2o3_memory/memory.h"
 #include "tiny_imageformat/tinyimageformat_base.h"
 #include "tiny_imageformat/tinyimageformat_query.h"
 #include "gfx_image/image.h"
 #include "gfx_image/create.h"
+
+AL2O3_EXTERN_C Image_ImageHeader const *Image_Create(uint32_t width,
+																										 uint32_t height,
+																										 uint32_t depth,
+																										 uint32_t slices,
+																										 TinyImageFormat format) {
+	auto image = Image_CreateNoClear(width, height, depth, slices, format);
+	if (image) {
+		memset(Image_RawDataPtr(image), 0, image->dataSize);
+	}
+
+	return image;
+}
+
+AL2O3_EXTERN_C Image_ImageHeader const *Image_CreateNoClear(uint32_t width,
+																														uint32_t height,
+																														uint32_t depth,
+																														uint32_t slices,
+																														TinyImageFormat format) {
+	if(width == 0)return nullptr;
+	if(height == 0) height = 1;
+	if(depth == 0) depth = 1;
+	if(slices == 0) slices = 1;
+
+	Image_ImageHeader tmp;
+	Image_FillHeader(width, height, depth, slices, format, &tmp);
+
+	auto *image = (Image_ImageHeader *) MEMORY_MALLOC(sizeof(Image_ImageHeader) + tmp.dataSize);
+	if (!image) { return nullptr; }
+
+	memcpy(image, &tmp, sizeof(Image_ImageHeader));
+
+	return image;
+}
+AL2O3_EXTERN_C void Image_FillHeader(uint32_t width,
+																		 uint32_t height,
+																		 uint32_t depth,
+																		 uint32_t slices,
+																		 TinyImageFormat format,
+																		 Image_ImageHeader *header) {
+
+
+	uint32_t const blockW = TinyImageFormat_WidthOfBlock(format);
+	uint32_t const blockH = TinyImageFormat_HeightOfBlock(format);
+	uint32_t const blockD = TinyImageFormat_DepthOfBlock(format);
+
+	// smallest sized a block texture can have is the block size
+	width = Math_MaxU32(width, blockW);
+	height = Math_MaxU32(height, blockH);
+	depth = Math_MaxU32(depth, blockD);
+
+	// round up to block size
+	width = (width + (blockW-1)) & ~(blockW-1);
+	height = (height + (blockH-1)) & ~(blockH-1);
+	depth = (depth + (blockD-1)) & ~(blockD-1);
+
+	uint64_t const pixelCount = (uint64_t)width * (uint64_t)height * (uint64_t)depth * (uint64_t)slices;
+	uint64_t const blockBitSize = TinyImageFormat_BitSizeOfBlock(format);
+	uint64_t const blockPixelCount = (uint64_t)blockW * (uint64_t)blockH * (uint64_t)blockD;
+	header->dataSize = (pixelCount * blockBitSize) / (blockPixelCount * 8ULL);
+
+	header->width = width;
+	header->height = height;
+	header->depth = depth;
+	header->slices = slices;
+	header->format = format;
+	header->flags = 0;
+	header->nextType = Image_NT_None;
+	header->nextImage = nullptr;
+}
+
+AL2O3_EXTERN_C Image_ImageHeader const* Image_CreateHeaderOnly(	uint32_t width,
+																																 uint32_t height,
+																																 uint32_t depth,
+																																 uint32_t slices,
+																																 TinyImageFormat format) {
+	auto *image = (Image_ImageHeader *) MEMORY_MALLOC(sizeof(Image_ImageHeader));
+	if (!image) { return nullptr; }
+	Image_FillHeader(width, height, depth, slices, format, image);
+
+	return image;
+}
+
+AL2O3_EXTERN_C void Image_Destroy(Image_ImageHeader const *image) {
+	if(!image) return;
+
+	// recursively free next in image chain
+	if(image->nextType != Image_NT_None) {
+		Image_Destroy(image->nextImage);
+	}
+
+	MEMORY_FREE((Image_ImageHeader*)image);
+}
 
 AL2O3_EXTERN_C Image_ImageHeader const * Image_Create1D(uint32_t width, enum TinyImageFormat format) {
   return Image_Create(width, 1, 1, 1, format);
